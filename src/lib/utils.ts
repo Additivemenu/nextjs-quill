62ee99f547ae95964bed2a87e9db4901c6131d94
XUeshuo Li <spoonlee24k@gmail.com>
2023-12-07 20:06:51 +1100

    FEAT-added button, color gradient decorator, product preview img

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import { twMerge } from "tailwind-merge";
-import clsx, { ClassValue } from "clsx";
-
-// This is a helper function to merge Tailwind classes with other classes
-export function cn(...inputs: ClassValue[]){
-    return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
